6267851870faf3ec213f8a70750eef9cffd09747
Chris Cassano <chris@litprotocol.com>
2022-01-13 13:10:16 -0500

    minimal example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { ShareModal } from "lit-access-control-conditions-modal";
 
 function App() {
+  const [showingModal, setShowingModal] = useState(false);
+  const showModal = () => {
+    setShowingModal(true);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={showModal}>Show Modal</button>
+      {showingModal ? (
+        <ShareModal
+          onClose={() => setShowingModal(false)}
+          onAccessControlConditionsSelected={(acc) =>
+            console.log("Access control conditions selected: ", acc)
+          }
+        />
+      ) : null}
     </div>
   );
 }
